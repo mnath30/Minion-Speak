359255aad623f5941e06ab2b6da699e3de514a0b
maitreyee nath <maitreyeenath30798@gmail.com>
2021-08-24 11:34:26 +0530

    Added the translation API

--- script.js
+++ script.js
@@ -2,10 +2,43 @@ const inputText=document.querySelector("#input-text");
 const btn=document.querySelector("#btn-translate");
 const result=document.querySelector("#result");
 
+var URL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var URL2="https://api.funtranslations.com/translate/minion.json"
+
+//function to create the URL with text
+function CreateURL(text){
+  var textURL=`${URL2}?text=${text}`;
+  return textURL;
+}
+
+//function to call the API
+function display(){
+  fetch(CreateURL(inputText.value))
+  .then(response=>response.json())
+  .then(json=>{result.innerText=json.contents.translated});
+
+}
+
+
+
 btn.addEventListener("click",display);
 
-function display(){ console.log(inputText.value);
-    result.innerText=inputText.value;
-}
+
+
+
+
+
+
+
+
+
+
+//resources
+// fetch('http://example.com/movies.json')
+//   .then(response => response.json())
+//   .then(data => console.log(data));
+
+  // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  // https://neog.camp/guide/lesson-four#assignment
 
 
